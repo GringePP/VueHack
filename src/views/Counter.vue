1678984c1adc025e556c93ee7975675ff9b7863b
Gringe920 <e-dam@163.com>
2018-12-13 23:01:49 +0800

    [+]: create counter endpoint

---
+++ src/views/Counter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="counter">
+    counter
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'counter',
+  data () {
+    return {
+      msg: 'counter'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
