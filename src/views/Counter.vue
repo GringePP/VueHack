7262be2be92210094c1a6c40d63f6b210ea3d1bb
Gringe920 <e-dam@163.com>
2019-01-13 21:59:22 +0800

    save

--- src/views/Counter.vue
+++ src/views/Counter.vue
@@ -7,17 +7,28 @@
     counter2 num: {{ $store.state.counter.count }}
     <button @click="increaseFun1">+</button>
     <button @click="decreaseFun2">-</button>
+
+
+
+
+
+    <vSwitch :value="value" :FId="FId" :index="index" class="vswitch" @changeSwitch="changeSwitch"></vSwitch>
   </div>
 </template>
 
 <script>
+import vSwitch from './Switch.vue'
 import {mapActions} from 'vuex'
 
 export default {
+  components: { vSwitch },
   name: 'counter',
   data () {
     return {
-      msg: 'counter'
+      msg: 'counter',
+      value: false,
+      FId: '123',
+      index: 0,
     }
   },
   methods: {
@@ -26,7 +37,10 @@ export default {
       decreaseFun: 'decrease',
       increaseFun1: 'increase',
       decreaseFun2: 'decrease',
-    })
+    }),
+    changeSwitch(checked,id,index){
+      console.log(index)
+    }
   }
 }
 </script>
